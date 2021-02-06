14002838037758c9117d28d019b87470769f5d05
Raymart <76593224+RaymartBediaFormalejo21@users.noreply.github.com>
2021-02-06 10:48:16 +0800

    Menu

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Supermarket System" << endl;
+    cout<<"\n\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+    cout<<"\n\t1. CUSTOMER";
+    cout<<"\n\t2. ADMINISTRATOR";
+    cout<<"\n\t3. EXIT";
+    cout<<"\n\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+
+    cout<<"\n\tOption: ";
     return 0;
 }
